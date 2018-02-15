9081fbd3258e9251351fcdcacd11a0bc921d25c9
Shinji Kato <hole.in1.kato@gmail.com>
2018-02-15 11:41:40 +0630

    Simplify

--- itp1_10b.py
+++ itp1_10b.py
@@ -5,11 +5,11 @@ import sys
 import math
 
 a, b, c = map(float, sys.stdin.readline().split())
-degree = abs(180-abs(c))*math.pi / 180
+degree = c*math.pi / 180
 
 height = abs(b * math.sin(degree))
-pos_x = b * math.cos(degree) + a
-edge_x = (height**2 + pos_x**2) ** 0.5
+pos_x = b * math.cos(degree)
+edge_x = (height**2 + (pos_x-a)**2) ** 0.5
 
 print(round(a * height / 2, 6))
 print(round(a + b + edge_x, 6))
