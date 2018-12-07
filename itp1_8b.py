c18b6f203f5679235596cba29cb52e93ee08a3c9
Shinji Kato <hole.in1.kato@gmail.com>
2018-12-07 10:29:40 +0630

    code reduction

--- itp1_8b.py
+++ itp1_8b.py
@@ -7,7 +7,4 @@ while True:
     s = sys.stdin.readline().strip()
     if s == '0':
         break
-    total = 0
-    for c in s:
-        total += int(c)
-    print(total)
+    print(sum(map(int, s)))
