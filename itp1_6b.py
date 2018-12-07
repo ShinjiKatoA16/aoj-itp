9e61aba6bcee8bc78c67400bcfacb4e2945ebcaa
Shinji Kato <hole.in1.kato@gmail.com>
2018-12-07 10:06:13 +0630

    use dict instead of class

--- itp1_6b.py
+++ itp1_6b.py
@@ -3,38 +3,20 @@
 
 import sys
 
-class suits():
-    def __init__(self, symbol):
-        self.exist = [False for i in range(14)]
-        self.symbol = symbol
+SUITS = ('S', 'H', 'C', 'D')
+cards = dict()
 
-    def mark_exist(self, rank):
-        self.exist[rank] = True
-
-    def is_exist(self, rank):
-        return self.exist[rank]
-
-spade = suits('S')
-heart = suits('H')
-club = suits('C')
-diamond = suits('D')
+for key in SUITS:
+    cards[key] = [False for i in range(14)]
 
 n = int(sys.stdin.readline())
 for i in range(n):
     suit, rank_s = sys.stdin.readline().split()
     rank = int(rank_s)
-    if suit == 'S':
-        spade.mark_exist(rank)
-    elif suit == 'H':
-        heart.mark_exist(rank)
-    elif suit == 'C':
-        club.mark_exist(rank)
-    elif suit == 'D':
-        diamond.mark_exist(rank)
-    else:
-        raise ValueError
 
-for s in (spade, heart, club, diamond):
+    cards[suit][rank] = True
+
+for suit in SUITS:
     for rank in range(1, 14):
-        if s.is_exist(rank) == False:
-            print(s.symbol, rank)
+        if cards[suit][rank] == False:
+            print(suit, rank)
